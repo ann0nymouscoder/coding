195e4a0cd6b56054ab2839e0e675c1ca37a78d46
ann0nymouscoder <165580569+ann0nymouscoder@users.noreply.github.com>
2024-04-05 00:44:10 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (40.14%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -1,23 +1,25 @@
+import java.util.Arrays;
+
 class Solution {
-    public int maxDepth(String s) 
-    {
-        int p=s.length();
-        int count=0;
-        int j=0;
-        int[] arr=new int[p];
-     for(int i=0;i<p;i++) 
-     {
-        if(s.charAt(i)=='(')
-        {
-            count++;
-            arr[j]=count;
-            j++;
+    public int maxDepth(String s) {
+        int p = s.length();
+        int count = 0;
+        int maxDepth = 0;
+
+        int[] arr = new int[p];
+        int j = 0;
+
+        for (int i = 0; i < p; i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+                arr[j] = count;
+                j++;
+                maxDepth = Math.max(maxDepth, count);
+            } else if (s.charAt(i) == ')') {
+                count--;
+            }
         }
-        else if(s.charAt(i)==')')
-        {
-            count--;
-        }
-     }  
-     return Arrays.stream(arr).max().getAsInt(); 
+
+        return maxDepth;
     }
 }
