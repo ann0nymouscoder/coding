91a69ec6408917748e6a1c547229a97a5992be6a
ann0nymouscoder <165580569+ann0nymouscoder@users.noreply.github.com>
2025-02-14 01:36:12 +0530

    Update Number of Common Factors.cpp

--- Number of Common Factors.cpp
+++ Number of Common Factors.cpp
@@ -1,17 +1,32 @@
-#include <algorithm> // For std::min
-
 class Solution {
-public:
-    int commonFactors(int a, int b) {
-        int count = 0;
-        int limit = std::min(a, b); // Find the smaller number
-        
-        for (int i = 1; i <= limit; ++i) {
-            if (a % i == 0 && b % i == 0) { // Check if 'i' is a common factor
-                ++count; // Increment count
+public :
+    int commonFactors(int a, int b) 
+    {
+        unordered_map<int,int> first;
+        int i=1;
+        int count=0;
+        while(i<=a)
+        {
+        if(a%i==0)
+        {
+            first[i]++;
+        }
+        i++;
+        } 
+        i=1;
+        while(i<=b)
+        {
+        if(b%i==0)
+        {
+            first[i]++;
+            if(first[i]>1)
+            {
+                count++;
             }
         }
-        
-        return count; // Return the total count of common factors
+        i++;
+        }
+        return count;
     }
+    
 };
