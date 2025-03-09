4bb71f28d53e13e21f3740b27f0109de75835b52
ann0nymouscoder <165580569+ann0nymouscoder@users.noreply.github.com>
2025-03-09 23:53:52 +0530

    Create Keyboard Row.cpp

---
+++ Keyboard Row.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> findWords(vector<string>& words) {
+     string s1="qwertyuiop";
+     string s2="asdfghjkl";
+     string s3="zxcvbnm";
+     vector<string>res;
+     map<char,int>mp;
+     for(auto &it:s1)mp[it]=1;
+     for(auto &it:s2)mp[it]=2;
+     for(auto &it:s3)mp[it]=3;
+     //for(auto &it:mp)cout<<it.first<<" "<<it.second<<" ";
+     for(auto &it:words){
+         int i;
+         for( i=1;i<it.size();i++){
+             if(mp[tolower(it[i])]!=mp[tolower(it[i-1])]){
+                // cout<<i<<" ";
+                 break;
+             }
+         }
+         if(i==it.size())res.push_back(it);
+     }
+     return res;
+    }
+};
