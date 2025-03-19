3e6daf00fc2c9518d69c8b46b3042a1d4523f62b
ann0nymouscoder <165580569+ann0nymouscoder@users.noreply.github.com>
2025-03-20 01:04:09 +0530

    Update Plus One.cpp

--- Plus One.cpp
+++ Plus One.cpp
@@ -19,3 +19,38 @@ public:
         return v;
     }
 };
+// or
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& v) 
+    {
+    int inc=0; 
+    for(int i=v.size()-1;i>=0;i--)
+    {
+        if(i==v.size()-1)
+        {
+            v[i]++;
+        }
+        if(v[i]==10)
+        {
+            v[i]=0;
+            inc=1;
+        }
+        else
+        {
+            v[i]=v[i]+inc;
+            if(v[i]==10)
+            {
+            v[i]=0;
+            inc=1;
+            }
+            else
+            {
+                inc=0;
+            }
+        }
+    }
+    if (inc) v.insert(v.begin(), 1);
+    return v;   
+    }
+};
