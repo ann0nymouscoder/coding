88dff13df919cd7199511fac34ac68f522c76565
ann0nymouscoder <165580569+ann0nymouscoder@users.noreply.github.com>
2025-04-02 23:19:28 +0530

    Create Maximum Value of an Ordered Triplet I.cpp

---
+++ Maximum Value of an Ordered Triplet I.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) 
+    {
+        long long count = 0;
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                for(int k = j + 1; k < n; k++)
+                {
+                    long long temp = (long long)(nums[i] - nums[j]) * nums[k];
+                    if (temp > count) count = temp;
+                }
+            }
+        } 
+        
+        return count;
+    }
+};
