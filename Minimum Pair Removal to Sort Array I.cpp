c9f76311f05c9c679bbbcaa1ec322e0b26985f98
ann0nymouscoder <165580569+ann0nymouscoder@users.noreply.github.com>
2025-04-07 23:53:38 +0530

    Create Minimum Pair Removal to Sort Array I.cpp

---
+++ Minimum Pair Removal to Sort Array I.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isSorted(const vector<int>& nums) {
+        for (int i = 1; i < nums.size(); ++i)
+            if (nums[i] < nums[i - 1]) return false;
+        return true;
+    }
+
+    int minimumPairRemoval(vector<int>& nums) {
+        int ops = 0;
+        while (!isSorted(nums)) {
+            int minSum = INT_MAX, idx = -1;
+            for (int i = 0; i < nums.size() - 1; ++i) {
+                int sum = nums[i] + nums[i + 1];
+                if (sum < minSum) {
+                    minSum = sum;
+                    idx = i;
+                }
+            }
+            nums[idx] = nums[idx] + nums[idx + 1];
+            nums.erase(nums.begin() + idx + 1);
+            ops++;
+        }
+        return ops;
+    }
+};
